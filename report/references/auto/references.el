a4acd2d5f2bc65cfb4e510cc591c5ffd97b4c722
ubunteous <46612154+Ubunteous@users.noreply.github.com>
2023-01-18 21:21:27 +0000

    Report added (partial)

---
+++ report/references/auto/references.el
@@ -0,0 +1,28 @@
+(TeX-add-style-hook
+ "references"
+ (lambda ()
+   (LaTeX-add-bibitems
+    "asimov_akinyemi_2000"
+    "tay"
+    "chatpgt-teach"
+    "chatgpt-detect"
+    "face_detect_bias"
+    "face_detect_disparity"
+    "face_detect_cnn"
+    "opencv_face_detect"
+    "ai_eco_impact"
+    "microsoft_buy_openai"
+    "captcha_recognition"
+    "captcha_original"
+    "gender_classify_children"
+    "gender_classify_facehop"
+    "emotion_detection_perceived"
+    "face_reading"
+    "face_recognition_svm"
+    "app_svm"
+    "thermal_cnn"
+    "cv_cancer"
+    "cv_robotics"
+    "recognition_algos"))
+ '(or :bibtex :latex))
+
